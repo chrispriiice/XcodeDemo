70c17b89cdfa6abcd23e63ab04ba03745fd9b924
Christopher Price <christopher.r.price@gmail.com>
2019-09-11 23:19:22 -0400

    just fooling around

--- XcodeDemo/ViewController.swift
+++ XcodeDemo/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+ let stuff = "just some things to test out source control"
 
 }
 
